6e52defae9d55d0d2be5cc13a6956cae71dabe87
diego123 <diego123@archlinux>
2025-11-21 12:53:03 -0600

    Prueba inicial sobre el commit...

---
+++ src/implementacion_de_grafo_j_m_m/Implementacion_De_Grafo_J_M_M.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package implementacion_de_grafo_j_m_m;
+
+/**
+ *
+ * @author diego123
+ */
+public class Implementacion_De_Grafo_J_M_M {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
