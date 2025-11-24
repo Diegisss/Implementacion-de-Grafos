7877504e86fd51cb82723dd615b05de8c75413ba
diego123 <diego123@archlinux>
2025-11-23 19:23:20 -0600

    Actualizada

--- src/Clases/Vertice.java
+++ src/Clases/Vertice.java
@@ -1,5 +1,8 @@
 package Clases;
 
+import static Clases.VerticeG.d;
+import java.awt.Graphics;
+
 /**
  *
  * @author diego123
@@ -8,10 +11,20 @@ public class Vertice {
 
     private String nombre;
     private int NumVertice;
+    
+    private int x, y;
+    public static final int d = 50;
+    
 
     public Vertice(String nombre) {
         this.nombre = nombre;
         this.NumVertice = -1;
+    }
+    
+    public Vertice (int x, int y, String nombre){
+        this.x = x;
+        this.y = y;
+        this.nombre = nombre;
     }
     
     public String getNombre(){
@@ -29,7 +42,20 @@ public class Vertice {
     public void asignarVertice(int x) {
         NumVertice = x;
     }
+    
+    public void pintar(Graphics g) {
+        g.drawOval(this.x - d / 2, this.y - d / 2, d, d);
+        g.drawString(nombre, x - nombre.length() * 3, y);
+    }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
     @Override
     public String toString() {
         return nombre + " (" + NumVertice + ")";
