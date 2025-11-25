ecf61c932d8d445af8594207fb059c7e571c6270
diego123 <diego123@archlinux>
2025-11-24 23:58:21 -0600

    Actualizacion final yeep :p

--- src/Clases/Arista.java
+++ src/Clases/Arista.java
@@ -1,31 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Clases;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 /**
  *
- * @author diego123
+ * @author diego jaimes escobar
  */
 public class Arista {
-    private Vertice des;
-    private int peso;
 
-    public Arista(Vertice des, int peso){
-        this.des = des;
-        this.peso = peso;
-    }
+    private Vertice des, ori;
+
 
     public Vertice getDes() {
         return des;
     }
 
-    public int getPeso() {
-        return peso;
+    public Vertice getOri() {
+        return ori;
     }
-    @Override
-    public String toString(){
-        return des.getNombre() + " peso (" + peso + ")";
+    //constructor
+    public Arista(Vertice ori, Vertice des) {
+        this.ori = ori;
+        this.des = des;
+    }
+    //diseño a las aristas y se dibujan
+    public void pintar(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g; //se aplica un casting a g de Graphics
+        g2.setColor(Color.BLUE); //se pinta de color azul
+        g2.setStroke(new BasicStroke(4)); //cambiar el grosor de las aristas
+        g2.drawLine(ori.getX(), ori.getY(), des.getX(), des.getY()); //dibujar la arista
+        
+
+        // se calcula el punto medio
+        int mx = (ori.getX() + des.getX()) / 2;
+        int my = (ori.getY() + des.getY()) / 2;
+        
+        int offset = 10; //la distancia que se tendra para el acumulamiento
+        if (ori.getY() < des.getY()) my += offset; //baja en el eje de las Y
+        else my -= offset; //sube en el eje de las Y
+       
+        //se colorea de color rojo las letras que acompañan a la arista (etiqueta)
+        g2.setColor(Color.RED);
+        g2.drawString(ori.getNombre() + " , " + des.getNombre(), mx, my);
+ 
+
     }
 }
